66e7646d272e846895db38f5c5a157812d4cc06d
Alejandro Mazzeo <ale.exactas@gmail.com>
2021-06-12 03:22:08 -0300

    Update the __init__.py file

--- arduscope/__init__.py
+++ arduscope/__init__.py
@@ -1,1 +1,10 @@
 from arduscope.arduscope import Arduscope, ArduscopeMeasure
+from arduscope.fit_tools import sine, fit_signal
+try:
+    from arduscope.wave_generator import WaveGenerator, WaveLoop
+except ModuleNotFoundError:
+    print("  NOTE: WaveGenerator requieres the extra package: simpleaudio\n"
+          "      Use `pip install simpleaudio`."
+          "      For Linux see simpleaudio dependecies at:\n"
+          "      https://simpleaudio.readthedocs.io"
+          "/en/latest/installation.html#linux-dependencies")
